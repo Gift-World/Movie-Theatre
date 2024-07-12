fdf1e450a403f5e5a7f42dc761b22dca1966afa4
charles Gift <charlesgiftangila@gmail.com>
2024-07-13 00:52:40 +0300

    initial commit

---
+++ script/movie.js
@@ -0,0 +1,49 @@
+function renderOneFilm(film) {
+  //Build a movie
+  let card = document.createElement("li");
+  card.className = "card";
+  card.innerHTML = `
+    <img src="${film.poster}">
+    <div class="content">
+    <h4 id="title"> ${film.title} </h4>
+    
+    <p id="capacity">Capacity: ${film.capacity} </p>
+    
+    
+    <p id="showtime"> ShowTime:${film.showtime}</p>
+    
+    
+    <p id="tickets">Available Tickets:${film.tickets_sold}</p>
+   
+    <p id="desc">${film.description}</p>
+    <button id="buyticket">Buy Ticket</button>
+    </div>
+    `;
+  //Add event listener to buy ticket button
+  card.querySelector("#buyticket").addEventListener("click", () => {
+    buyTicket(film, card);
+  });
+  //add animal card to dom
+  document.querySelector("#film-list").appendChild(card);
+}
+function buyTicket(film, card) {
+  const ticketsElements = card.querySelector("#tickets");
+  let availableTickets = film.capacity - film.tickets_sold;
+  if (availableTickets > 0) {
+    availableTickets -= 1;
+    film.tickets_sold += 1;
+    ticketsElements.textContent = `Available Tickets:${availableTickets}`;
+  } else {
+    alert("Sorry, this show is sold out!");
+  }
+}
+//fetching
+function getAllFilms() {
+  fetch("http://localhost:3000/films")
+    .then((res) => res.json())
+    .then((filmData) => filmData.forEach((film) => renderOneFilm(film)));
+}
+function initialize() {
+  getAllFilms();
+}
+initialize();
